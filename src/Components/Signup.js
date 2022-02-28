96520d5f7ff0bdf31610d6ba068b707e62c080f7
Tymee Kong <tymeekong562@gmail.com>
2022-02-28 12:07:56 -0800

    Added sign up form

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Signup() {
-    return <div>sign up page</div>;
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [repassword, setRepassword] = useState();
+
+    function handleChange(event) {
+        event.preventDefault();
+        //I feel like I can shorten this but I'm not sure how. Brain fried
+        if (event.target.name === 'user') {
+            setUsername(event.target.value);
+        } else if (event.target.name === 'password') {
+            setPassword(event.target.value);
+        } else {
+            setRepassword(event.target.value);
+        }
+    }
+
+    function handleClick(event) {
+        event.preventDefault();
+        password === repassword
+            ? console.log('Created account')
+            : console.log('Passwords don`t match');
+    }
+    return (
+        <div>
+            <form onChange={handleChange}>
+                <label htmlFor='user'>Username</label>
+                <br />
+                <input type='text' name='user' />
+                <br />
+                <label htmlFor='password'>Password</label>
+                <br />
+                <input type='password' name='password' />
+                <br />
+                <label htmlFor='reenter'>Reenter password</label>
+                <br />
+                <input type='password' name='reenter' />
+                <br />
+                <button>Create account</button>
+            </form>
+        </div>
+    );
 }
 
 export default Signup;
