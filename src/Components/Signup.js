5aa1239019868380a06200d5db6bbc21db48c008
Tymee Kong <tymeekong562@gmail.com>
2022-03-03 15:06:47 -0800

    Added styled-components library and centered form for signup page and added box-shadow

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
+import { PageContainer, FormContainer } from '../Styling/Signup.Style';
 
 function Signup() {
     const schema = Yup.object().shape({
@@ -22,50 +23,54 @@ function Signup() {
         console.log(data.password);
     };
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor='user'>User name</label>
-                <br />
-                <input
-                    name='user'
-                    type='text'
-                    {...register('username', { required: true })}
-                />
-                <br />
-                <div className='form-group'>
-                    <label>Password</label>
+        <PageContainer>
+            <FormContainer>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor='user'>User name</label>
                     <br />
                     <input
-                        name='password'
-                        type='password'
-                        {...register('password')}
-                        className={`form-control ${
-                            errors.password ? 'is-invalid' : ''
-                        }`}
+                        name='user'
+                        type='text'
+                        {...register('username', { required: true })}
                     />
-                    <div className='invalid-feedback'>
-                        {errors.password?.message}
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor='confirmPassword'>Confirm password</label>
                     <br />
-                    <input
-                        name='confirmPassword'
-                        type='password'
-                        {...register('confirmPassword')}
-                        className={`form-control ${
-                            errors.confirmPassword ? 'is-invalid' : ''
-                        }`}
-                    />
-                    <div className='invalid-feedback'>
-                        {errors.confirmPassword?.message}
+                    <div className='form-group'>
+                        <label>Password</label>
+                        <br />
+                        <input
+                            name='password'
+                            type='password'
+                            {...register('password')}
+                            className={`form-control ${
+                                errors.password ? 'is-invalid' : ''
+                            }`}
+                        />
+                        <div className='invalid-feedback'>
+                            {errors.password?.message}
+                        </div>
                     </div>
-                </div>
+                    <div>
+                        <label htmlFor='confirmPassword'>
+                            Confirm password
+                        </label>
+                        <br />
+                        <input
+                            name='confirmPassword'
+                            type='password'
+                            {...register('confirmPassword')}
+                            className={`form-control ${
+                                errors.confirmPassword ? 'is-invalid' : ''
+                            }`}
+                        />
+                        <div className='invalid-feedback'>
+                            {errors.confirmPassword?.message}
+                        </div>
+                    </div>
 
-                <input type='submit' />
-            </form>
-        </div>
+                    <input type='submit' />
+                </form>
+            </FormContainer>
+        </PageContainer>
     );
 }
 Signup.propTypes = {
---
+++ src/Styling/Signup.Style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const FormContainer = styled.div`
+    padding: 50px;
+    box-shadow: 5px 5px 10px 2px #afc0db;
+`;
+
+export const BoldLabel = styled.label``;
