89076767e2e9ad02ab945c0e2b06bba7a724c6d7
Tymee Kong <tymeekong562@gmail.com>
2022-03-14 14:02:16 -0700

    Added image and changed background color of NavContainer and ContentContainer

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -3,14 +3,15 @@ import {
     NavLink,
     NavContainer,
     ContentContainer,
+    DiceImage,
+    ElementDiv,
+    PurchaseButton,
 } from '../Styling/Homepage.Style';
-// import '../Styling/reset.css';
-
 function Homepage() {
     return (
         <>
             <NavContainer>
-                <h1>Tabletop website</h1>
+                <h1>Dungeons and Dragons</h1>
                 <ul>
                     <NavLink to='/login'>Login</NavLink>
                     <NavLink to='/signup'>Signup</NavLink>
@@ -19,12 +20,27 @@ function Homepage() {
             <ContentContainer>
                 {/* I want an image here showing the dnd handbook*/}
                 {/* I want to have some text showing off that it's dnd */}
-                <div>
-                    <h1>Dnd is life</h1>
-                </div>
-                <div>
-                    <h1>Test</h1>
-                </div>
+                <ElementDiv>
+                    <h1>Welcome to our Dungeons and Dragons website!</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicingS
+                        elit. Delectus dolor numquam eos provident porro
+                        pariatur veritatis laboriosam quam, a quisquam
+                        reiciendis omnis iusto dolorum error enim culpa modi
+                        molestiae incidunt.
+                    </p>
+                    <form
+                        action='https://www.amazon.com/Players-Handbook-Dungeons-Dragons-Wizards/dp/0786965606
+                    
+                    '
+                        target='_blank'
+                    >
+                        <PurchaseButton>Purchase the handbook!</PurchaseButton>
+                    </form>
+                </ElementDiv>
+                <ElementDiv>
+                    <DiceImage src={require('../Images/dice.jpg')} alt='Dice' />
+                </ElementDiv>
             </ContentContainer>
         </>
     );
--- src/Styling/Homepage.Style.js
+++ src/Styling/Homepage.Style.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+// Hex colors
+
+// #5B8266
+// #2F2504
+// #EAB464
+// #A7754D Original background color of NavContainer and ContentContainer
+// #403233
+
 export const NavLink = styled(Link)`
     font-size: 35px;
     text-decoration: none;
@@ -10,12 +18,43 @@ export const NavLink = styled(Link)`
 `;
 
 export const NavContainer = styled.nav`
-    background-color: yellow;
+    background-color: #ffe4c4;
     display: flex;
+    color: black;
     justify-content: space-between;
 `;
 
 export const ContentContainer = styled.div`
     display: flex;
-    justify-content: space-around;
+    background-color: #ffe4c4;
+    justify-content: space-evenly;
 `;
+
+export const DiceImage = styled.img`
+    padding-top: 30px;
+    max-width: 400px;
+    margin-bottom: 50px;
+`;
+
+export const ElementDiv = styled.div`
+    max-width: 400px;
+    /* margin-top: 40px; */
+    /* color: #08415c; */
+    color: black;
+`;
+
+export const PurchaseButton = styled.button`
+    height: 50px;
+    width: 120px;
+    background-color: #eab464;
+    border: 1px solid #2f2504;
+    color: black;
+    margin-right: 30px;
+    cursor: pointer;
+`;
+
+// #5B8266
+// #2F2504
+// #EAB464
+// #A7754D
+// #403233
